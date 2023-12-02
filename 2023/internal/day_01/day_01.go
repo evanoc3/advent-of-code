cfd606371a9462a4cfd0421632deabdca28afc8e
Evan O'Connor <evanoc3@gmail.com>
2023-12-02 12:06:16 +0000

    Finished day 01 part 2

--- 2023/internal/day_01/day_01.go
+++ 2023/internal/day_01/day_01.go
@@ -16,12 +16,12 @@ func Solve() {
 }
 
 
-func Part1(input string) uint64 {
+func Part1(input string) int {
 	lines := strings.Split(input, "\n")
 
-	incrementor := uint64(0)
+	incrementor := 0
 	for _, line := range lines {
-		var firstDigit, lastDigit uint64
+		var firstDigit, lastDigit int
 
 		for _, c := range line {
 			if !unicode.IsDigit(c) {
@@ -29,10 +29,60 @@ func Part1(input string) uint64 {
 			}
 			
 			if firstDigit == 0 {
-				firstDigit = uint64(c - '0')
+				firstDigit = int(c - '0')
 			}
 
-			lastDigit = uint64(c - '0')
+			lastDigit = int(c - '0')
+		}
+
+		totalNum := (firstDigit * 10) + lastDigit
+		fmt.Printf("(%d) (%d) (%d)\n", firstDigit, lastDigit, totalNum)
+
+		incrementor += totalNum
+	}
+
+	return incrementor
+}
+
+
+func Part2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	incrementor := 0
+	for _, line := range lines {
+		var firstDigit, lastDigit int
+		var setDigit = func(digit int) {
+			if firstDigit == 0 {
+				firstDigit = digit
+			}
+
+			lastDigit = digit
+		}
+
+		for i, c := range line {
+			if unicode.IsDigit(c) {
+				setDigit(int(c - '0'))
+			} else {
+				if i <= len(line) - 3 && line[i:i+3] == "one" {
+					setDigit(1)
+				} else if i <= len(line) - 3 && line[i:i+3] == "two" {
+					setDigit(2)
+				} else if i <= len(line) - 5 && line[i:i+5] == "three" {
+					setDigit(3)
+				} else if i <= len(line) - 4 && line[i:i+4] == "four" {
+					setDigit(4)
+				} else if i <= len(line) - 4 && line[i:i+4] == "five" {
+					setDigit(5)
+				} else if i <= len(line) - 3 && line[i:i+3] == "six" {
+					setDigit(6)
+				} else if i <= len(line) - 5 && line[i:i+5] == "seven" {
+					setDigit(7)
+				} else if i <= len(line) - 5 && line[i:i+5] == "eight" {
+					setDigit(8)
+				} else if i <= len(line) - 4 && line[i:i+4] == "nine" {
+					setDigit(9)
+				}
+			}
 		}
 
 		totalNum := (firstDigit * 10) + lastDigit
--- 2023/internal/day_01/day_01_test.go
+++ 2023/internal/day_01/day_01_test.go
@@ -7,16 +7,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-//go:embed sample_input.txt
-var sample_input string
+//go:embed part1_sample_input.txt
+var part1_sample_input string
+//go:embed part2_sample_input.txt
+var part2_sample_input string
 
 
 func TestPart1(t *testing.T) {
 	require := require.New(t)
 
 	// test on smaple input
-	require.EqualValues(142, Part1(sample_input))
+	require.Equal(142, int(Part1(part1_sample_input)))
 
 	// test on real input
-	require.EqualValues(56397, Part1(input))
+	require.Equal(56397, int(Part1(input)))
+}
+
+
+func TestPart2(t *testing.T) {
+	require := require.New(t)
+
+	//test on sample input
+	require.Equal(281, Part2(part2_sample_input))
+
+	// test on real input
+	require.Equal(55701, Part2(input))
 }
