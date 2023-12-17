b0de8fb3dc86fb6dba0dc691463d8730bcf8b044
Evan O'Connor <evanoc3@gmail.com>
2023-12-17 16:03:58 +0000

    Solved 2023 Day 3 Part 2

--- 2023/internal/day_03/day_03.go
+++ 2023/internal/day_03/day_03.go
@@ -14,6 +14,7 @@ var input string
 
 func Solve() {
 	fmt.Printf("day_03.Part1: %d\n", Part1(input))
+	fmt.Printf("day_03.Part2: %d\n", Part2(input))
 }
 
 
@@ -42,9 +43,29 @@ func (pn *partNumber) Int() int {
 	return int(i)
 }
 
-
-
 func Part1(input string) int {
+	inp := parseInput(input)
+
+	var acc int
+	for _, pn := range inp.partNumbers {
+		// define the rectangle around the part number which we cehck for a symbol within
+		x1 := pn.X - 1
+		x2 := pn.X + len(pn.Value)
+		y1 := pn.Y - 1
+		y2 := pn.Y + 1
+
+		for _, sym := range inp.symbolCoords {
+			if sym.X >= x1 && sym.X <= x2 && sym.Y >= y1 && sym.Y <= y2 {
+				acc += pn.Int()
+				break
+			}
+		}
+	}
+
+	return acc
+}
+
+func parseInput(input string) parsedInput {
 	lines := strings.Split(input, "\n")
 
 	inp := parsedInput{
@@ -92,21 +113,89 @@ func Part1(input string) int {
 		}
 	}
 
-	var acc int
-	for _, pn := range inp.partNumbers {
-		// define the rectangle around the part number which we cehck for a symbol within
-		x1 := pn.X - 1
-		x2 := pn.X + len(pn.Value)
-		y1 := pn.Y - 1
-		y2 := pn.Y + 1
+	return inp
+}
 
-		for _, sym := range inp.symbolCoords {
-			if sym.X >= x1 && sym.X <= x2 && sym.Y >= y1 && sym.Y <= y2 {
-				acc += pn.Int()
-				break
-			}
-		}
+
+func Part2(input string) int {
+	inp := parseInput(input)
+	gears := getGears(inp)
+
+	var acc int
+	for _, gear := range gears {
+		acc += gear.AdjacentNumbers[0].Int() * gear.AdjacentNumbers[1].Int()
 	}
 
 	return acc
+}
+
+
+type gear struct {
+	Symbol *symbol
+	AdjacentNumbers []*partNumber
+}
+
+type rect struct {
+	coord
+	W int
+	H int
+}
+
+func getGears(inp parsedInput) []*gear {
+	gears := make([]*gear, 0)
+
+	for _, sym := range inp.symbolCoords {
+		if sym.Symbol != '*' {
+			continue
+		}
+
+		symRect := rect{
+			coord: coord{
+				X: sym.X - 1,
+				Y: sym.Y - 1,
+			},
+			W: 3,
+			H: 3,
+		}
+
+		adjacentParts := make([]*partNumber, 0)
+
+		for _, pn := range inp.partNumbers {
+			pnRect := rect{
+				coord: coord{
+					X: pn.X,
+					Y: pn.Y,
+				},
+				W: len(pn.Value),
+				H: 1,
+			}
+
+			if rectsIntersect(symRect, pnRect) {
+				adjacentParts = append(adjacentParts, pn)
+			}
+		}
+
+		if len(adjacentParts) == 2 {
+			gears = append(gears, &gear{
+				Symbol: sym,
+				AdjacentNumbers: adjacentParts,
+			})
+		}
+	}
+
+	return gears
+}
+
+func rectsIntersect(rect1, rect2 rect) bool {
+	x1 := rect1.X
+	x2 := rect1.X + rect1.W
+	y1 := rect1.Y
+	y2 := rect1.Y + rect1.H
+
+	x3 := rect2.X
+	x4 := rect2.X + rect2.W
+	y3 := rect2.Y
+	y4 := rect2.Y + rect2.H
+
+	return x1 < x4 && x2 > x3 && y1 < y4 && y2 > y3
 }
--- 2023/internal/day_03/day_03_test.go
+++ 2023/internal/day_03/day_03_test.go
@@ -14,13 +14,26 @@ var sampleInput string
 func TestPart1WithSampleInput(t *testing.T) {
 	require := require.New(t)
 
-	// test against sample input
 	require.Equal(4361, Part1(sampleInput))
 }
+
 
 func TestPart1WithActualInput(t *testing.T) {
 	require := require.New(t)
 
-	// test against sample input
 	require.Equal(536202, Part1(input))
+}
+
+
+func TestPart2WithSampleInput(t *testing.T) {
+	require := require.New(t)
+
+	require.Equal(467835, Part2(sampleInput))
+}
+
+
+func TestPart2WithActualInput(t *testing.T) {
+	require := require.New(t)
+
+	require.Equal(78272573, Part2(input))
 }
